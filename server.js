7257673299e3c6d2a70c0275a9152df966318d11
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2026-02-23 14:30:09 +0200

    Adding /jobs route

--- server.js
+++ server.js
@@ -2,14 +2,23 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require('cors');
+const jobsData = require('./jobs.json');
 
 const PORT = process.env.SERVER_PORT || 5000;
 
 const server = express();
 
+// Middleware
+server.use(cors());
+
 // Routes
 server.get("/", (req, res) => {
   res.send("<h1>JOBS BOARD API</h1>");
+});
+
+server.get('/jobs', (req, res) => {
+  res.json( jobsData);
 });
 
 // Start the server
